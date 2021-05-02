35304f750cc07cee33d020928462ef166d47f1d0
mickmccann <mickmccann@ymail.com>
2021-05-02 14:10:52 +0000

    Added a session user profile page

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def register():
         # put the user into a temporary page session
         session["user"] = request.form.get("username").lower()
         flash("Registration Successful!")
+    return redirect(url_for("profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -62,7 +63,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 # invaild password
                 flash("Oops! You've entered an incorrect Username and/or Password.")
@@ -73,6 +77,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # grabs the user's username from the database
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":

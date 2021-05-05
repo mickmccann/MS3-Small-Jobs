02e1f7d12bb85c4b2a0f4784a7a1b1a8eb6cfde4
mickmccann <mickmccann@ymail.com>
2021-05-05 20:33:02 +0000

    Created add_jobs template with appropiate routing

--- app.py
+++ app.py
@@ -99,6 +99,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_jobs")
+def add_jobs():
+    return render_template("add_jobs.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
